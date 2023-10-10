2d2fc72ae7a73acbbbfc33b3d42b2a6fb4e3282b
arnau <amarques20733@iesjoanramis.org>
2023-10-10 18:57:49 +0200

    Modificacio AvaluadorExpressions

---
+++ AvaluadorExpressions.java
@@ -0,0 +1,21 @@
+package provaarnau;
+
+public class AvaluadorExpressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//Modificacio de programa
+		int a=5;
+		int b=9;
+		int c=3;
+		int d=4;
+		
+		System.out.println(2 - a * b + c);
+		System.out.println((2 - a) * b + c);
+		System.out.println(a * b - c * a - d);
+		System.out.println(a / 3 - b);
+		System.out.println(a / (33 - b));
+		System.out.println(d * 23 - 1 + b);
+	}
+
+}

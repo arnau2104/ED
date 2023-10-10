9021db4b84dcac6d9809d1fad7a91f50cc4491a2
arnau <amarques20733@iesjoanramis.org>
2023-10-10 19:06:28 +0200

    Modificacio multiplicacio

--- AvaluadorExpressions.java
+++ AvaluadorExpressions.java
@@ -7,15 +7,9 @@ public class AvaluadorExpressions {
 	//Modificacio de programa
 		int a=5;
 		int b=9;
-		int c=3;
-		int d=4;
 		
-		System.out.println(2 - a * b + c);
-		System.out.println((2 - a) * b + c);
-		System.out.println(a * b - c * a - d);
-		System.out.println(a / 3 - b);
-		System.out.println(a / (33 - b));
-		System.out.println(d * 23 - 1 + b);
+		
+		System.out.println( a * b);
 	}
 
 }

0536a6ce5b8de59aebb0bfc3bec60394f3b83545
arnau <amarques20733@iesjoanramis.org>
2023-10-20 18:00:59 +0200

    Commit a Modificacio

--- provaGit/src/provaGit/Exercici5Git.java
+++ provaGit/src/provaGit/Exercici5Git.java
@@ -4,7 +4,7 @@ public class Exercici5Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola mundo");
+System.out.println("hola mundo modificat");
 	}
 
 }
